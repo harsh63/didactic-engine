22be32046d492657d52910fddd9f4f9e4dea8c8e
Harsg63 <hsagar63@gmail.com>
2021-03-27 22:35:53 +0530

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  full_name: 'Avani Tavargeri',
+  email: 'avani@zenmind.com',
+  password: '123456',
+  company_name: 'Zenmind Studio',
+  company_address: '9, Untwadi Rd, Madhura Park, Tidke Nagar, Nashik, Maharashtra 422008',
+  role: :user,
+  phone_number: '+917456564578'
+)
+
+User.create(
+  full_name: 'Akshat Kedia',
+  email: 'akshat@zenmind.com',
+  password: '123456',
+  company_name: 'Zenmind Studio',
+  company_address: '9, Untwadi Rd, Madhura Park, Tidke Nagar, Nashik, Maharashtra 422008',
+  role: :user,
+  phone_number: '+916788867689'
+)
+
+User.create(
+  full_name: 'Harsh Sagar',
+  email: 'harsh@zenmind.com',
+  password: '123456',
+  company_name: 'Zenmind Studio',
+  company_address: '9, Untwadi Rd, Madhura Park, Tidke Nagar, Nashik, Maharashtra 422008',
+  role: :admin,
+  phone_number: '+918516906963'
+)
